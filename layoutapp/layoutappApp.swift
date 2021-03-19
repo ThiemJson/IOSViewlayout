bf3deda6b7835d6af3a38b3c45d59de38985e7a0
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-03-19 23:28:40 +0700

    Initial Commit

---
+++ layoutapp/layoutappApp.swift
@@ -0,0 +1,17 @@
+//
+//  layoutappApp.swift
+//  layoutapp
+//
+//  Created by Thiem Jason on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct layoutappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
