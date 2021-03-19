17d8ef5f7d67dc4c5a7532956f1a91d6656cc592
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-03-20 01:45:34 +0700

    Practise with State management

--- layoutapp/ContentView.swift
+++ layoutapp/ContentView.swift
@@ -1,21 +1,73 @@
-//
-//  ContentView.swift
-//  layoutapp
-//
-//  Created by Thiem Jason on 3/19/21.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+// Our observable object class
+class Person: ObservableObject {
+    @Published var age = 0
+}
+
+
+struct ContentView : View {
+    var body: some View{
+        let person = Person()
+        return RootView(person: person)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct RootView: View {
+    @StateObject var person : Person
+    
+    var body: some View {
+        TabView(selection: .constant(2),
+                content:  {
+                    DemoView(screenName: "Home")
+                        .tabItem {
+                            Image.init(systemName: "star.fill")
+                            Text("Home")
+                        }.tag(1)
+                    DemoView( screenName: "Result")
+                        .tabItem {
+                            Image.init(systemName: "star.fill")
+                            Text("Result")
+                        }.tag(2)
+                    DemoView(screenName: "More")
+                        .tabItem {
+                            Image.init(systemName: "star.fill")
+                            Text("More")
+                        }.tag(3)
+                })
+            .environmentObject(person)
+    }
+}
+
+struct DemoView1 : View {
+    @EnvironmentObject var person : Person
+    var body: some View {
+        VStack{
+            Text("In demoview1 \(person.age)")
+        }
+        .onAppear(){
+            print("==> \(person.age)")
+        }
+    }
+    
+}
+
+struct DemoView:View {
+    @EnvironmentObject var person : Person
+    @State var text:String = ""
+    var screenName:String
+    var body: some View {
+        VStack{
+            Text(screenName)
+                .font(.title)
+            
+            TextField("Buggy Keyboard Issue \(person.age)", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            DemoView1()
+        }
+        .padding()
+        .onAppear(perform: {
+            debugPrint("OnAppear of : \(screenName)")
+        })
     }
 }
